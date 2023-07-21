a06219172bb1c5aa42adaeeadbf9004d84959576
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-21 21:15:38 +0200

    modify bubble_sort.c file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,19 +8,19 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, l;
-    int temp;
-    for (l = 0; l < size; l++)
-    {
-        for (i = 0; i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+size_t i, l;
+int temp;
+for (l = 0; l < size; l++)
+{
+for (i = 0; i < size - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+temp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
