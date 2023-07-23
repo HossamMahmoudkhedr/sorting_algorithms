bb21c61a93aeb2f711bb95ba8bb9c95a510f8692
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-23 16:10:53 +0200

    Modify counting sort funciton

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,53 +1,59 @@
 #include "sort.h"
+#include <stdlib.h>
 
 /**
- * counting_sort - sort the array using counting sort algorithm
- *
- * @array: the array to be sorted
- * @size: the size of the array
- * Return: void
+ * arr_max - array max
+ * @array: array
+ * @size: size of the array
+ * Return: max
  */
+int arr_max(int *array, size_t size)
+{
+int max;
+size_t i;
+
+max = array[0];
+for (i = 1; i < size; i++)
+if (array[i] > max)
+max = array[i];
+return (max);
+}
+
+/**
+* counting_sort - sorts an array with the Counting sort algorithm
+* @array: array to sort
+* @size: size of the array
+*/
 void counting_sort(int *array, size_t size)
 {
-int *countArr;
-int *sortedArr;
-size_t i, j;
+int *arr, *o_arr, max, num;
+size_t i;
 
-countArr = (int*)malloc(sizeof(int) * (size + 1));
-sortedArr = (int*)malloc(sizeof(int) * (size + 1));
+if (size < 2 || !array)
+return;
+max = arr_max(array, size);
+
+arr = malloc(sizeof(size_t) * (max + 1));
+o_arr = malloc(sizeof(int) * size);
+
+for (i = 0; (int)i <= max; i++)
+arr[i] = 0;
 for (i = 0; i < size; i++)
 {
-countArr[i] = 0;
-sortedArr[i] = 0;
+num = array[i];
+arr[num] += 1;
 }
-
-print_array(countArr, size);
-for (i = 0; i < size - 1; i++)
-{
-for (j = i + 1; j < size; j++)
-{
-if (array[i] < array[j])
-{
-countArr[j] += 1;
-}
-else
-{
-countArr[i] += 1;
-}
-}
-print_array(countArr, size);
-}
-
+for (i = 1; (int)i <= max; i++)
+arr[i] += arr[i - 1];
+print_array(arr, max + 1);
 for (i = 0; i < size; i++)
 {
-sortedArr[countArr[i]] = array[i];
+o_arr[arr[array[i]] - 1] = array[i];
+arr[array[i]]--;
 }
-
 for (i = 0; i < size; i++)
-{
-array[i] = sortedArr[i];
-}
+array[i] = o_arr[i];
 
-free(countArr);
-free(sortedArr);
+free(o_arr);
+free(arr);
 }
