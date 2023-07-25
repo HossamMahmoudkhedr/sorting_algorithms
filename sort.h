fc00f95112c467707207adf6706d6900f20cd94d
IBruteDude <kamarf505@gmail.com>
2023-07-25 18:56:46 +0200

    Updated sort.h

--- sort.h
+++ sort.h
@@ -2,7 +2,10 @@
 #define SORTING_ALGORITHMS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <limits.h>
+#include <math.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,4 +25,13 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+
+void heap_sort(int *array, size_t size);
+
+void bitonic_sort(int *array, size_t size);
+
+
 #endif /* SORTING_ALGORITHMS */
